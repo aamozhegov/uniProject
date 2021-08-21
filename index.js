aaabaab68987d345667715c9b7ee37e00377ddd5
Artem Mozhegov <amozhegov@yahoo.com>
2021-08-22 02:18:59 +0300

    Created error.html, styles.css

--- index.js
+++ index.js
@@ -4,33 +4,50 @@ const path = require('path')
 
 const server = http.createServer((req, res) => {
 
+let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
+const ext = path.extname(filePath)
+let contentType = 'text/html'
 
-	if (req.url === '/') {
-		fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
-			if (err) {
-				throw err
-			}
-			res.writeHead(200, {
-				'Content-Type': 'text/html'
+switch (ext) {
+	case '.css':
+		contentType = 'text/css'
+		break
+	case '.js':
+		contentType = 'text/javascript'
+		break
+	default:
+		contentType = 'text/html'
+}
+
+if (!ext) {
+	filePath += '.html'
+}
+
+	fs.readFile(filePath, (err, content) => {
+		if (err) {
+			fs.readFile(path.join(__dirname, 'public', 'error.html'), (err, data) => {
+				if (err) {
+					res.writeHead(500)
+					res.end()
+				} else {
+					res.writeHead(200, {
+						'Content-Type' : contentType
+					})
+					res.end(data)
+				}
 			})
-			 res.end(data)
+	} else {
+		res.writeHead(200, {
+			'Content-Type' : 'text/html'
 		})
+		res.end(content)
+	
 	}
-
-	else if (req.url === '/contact') {
-		fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, data) => {
-			if (err) {
-				throw err
-			}
-			res.writeHead(200, {
-				'Content-Type': 'text/html'
-			})
-			res.end(data)
-		})
-	}
-
+})
 })
 
-server.listen(3000, () => {
-	console.log('Server has been started')
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, () => {
+	console.log(`Server has been started on ${PORT}...`)
 })
